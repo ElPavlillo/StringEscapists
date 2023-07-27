d18be8fd73e0db45739a23b602460eafd6ffa4ba
ElPavlillo <elpavlillo@gmail.com>
2023-07-27 19:44:40 +0200

    Minimal version

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function StringEscapist() {
+    const str = document.getElementById("string");
+    const result = document.getElementById("result");
+    const selectedChar = document.getElementById("searchChar").value;
+    const additionalChar = document.getElementById("escapeChar").value;
+
+    if (selectedChar && additionalChar) {
+        const regex = new RegExp(selectedChar, "g");
+        const updatedText = str.value.replace(regex, additionalChar + selectedChar);
+        result.value = updatedText;
+    }
+  }
